4e838531dbbf31d9a5f35aed9561499c2bffa55f
H1022 <pennpellwc@gmail.com>
2021-06-25 12:08:18 +0200

    manytomanyrelations

---
+++ mybooks2/Migrations/20210623094559_PublisherAdded.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mybooks2.Migrations
+{
+    public partial class PublisherAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PublisherId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Publisher",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publisher", x => x.Id);
+                });
+
+      
+
+ 
+        }
+    }
+}
